13a74e77899381c7fe9dfa9c9793265fdf3d9228
Shripad Shirsat <shripadshirsat@outlook.com>
2021-01-17 17:42:09 +0530

    1st Commit

---
+++ CRUDMVCwithouEF/Controllers/BookController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CRUDMVCwithouEF.Data;
+using CRUDMVCwithouEF.Models;
+
+namespace CRUDMVCwithouEF.Controllers
+{
+    public class BookController : Controller
+    {
+       
+
+        public BookController()
+        {
+            
+        }
+
+        // GET: Book
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+
+        // GET: Book/AddOrEdit/
+        public  IActionResult AddOrEdit(int? id)
+        {
+            BookViewModel bookViewModel = new BookViewModel();
+
+            return View(bookViewModel);
+        }
+
+        // POST: Book/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddOrEdit(int id, [Bind("BookID,Title,Author,Price")] BookViewModel bookViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bookViewModel);
+        }
+
+        // GET: Book/Delete/5
+        public  IActionResult Delete(int? id)
+        {
+            
+
+            return View();
+        }
+
+        // POST: Book/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+           
+            return RedirectToAction(nameof(Index));
+        }
+
+      
+    }
+}
---
+++ CRUDMVCwithouEF/Data/CRUDMVCwithouEFContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CRUDMVCwithouEF.Models;
+
+namespace CRUDMVCwithouEF.Data
+{
+    public class CRUDMVCwithouEFContext : DbContext
+    {
+        public CRUDMVCwithouEFContext (DbContextOptions<CRUDMVCwithouEFContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CRUDMVCwithouEF.Models.BookViewModel> BookViewModel { get; set; }
+    }
+}
